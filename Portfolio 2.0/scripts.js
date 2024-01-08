b7a545e189e8f33ece9cd5663b12ce60e7c79969
Yvelmence <132332290+Yvelmence@users.noreply.github.com>
2024-01-08 22:12:25 +0800

    Add files via upload
    reupload to attempt to fix js not working

---
+++ Portfolio 2.0/scripts.js
@@ -0,0 +1,23 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabName){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabName).classList.add("active-tab");
+}
+
+
+var sideMenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sideMenu.style.right = "0";
+}
+function closemenu(){
+    sideMenu.style.right = "-200px";
+}
